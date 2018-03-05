768ec7adcbc005d65530d29f67db3a0db1b61ef9
Alex Globa <grobych@gmail.com>
2018-03-05 16:24:08 +0300

    add Team and TeamViewMAnager classes

---
+++ MobileProjectManager/MobileProjectManager/Models/Team.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace MobileProjectManager.Models
+{
+    class Team
+    {
+        [BsonId]
+        ObjectId Id { get; set; }
+
+        string Name { get; set; }
+
+        ObjectId ManagerID { get; set; }
+
+        Collection<ObjectId> WorkersId { get; set; }
+    }
+}
---
+++ MobileProjectManager/MobileProjectManager/ViewModels/TeamViewModel.cs
@@ -0,0 +1,63 @@
+﻿using MobileProjectManager.Models;
+using MobileProjectManager.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace MobileProjectManager.ViewModels
+{
+    class TeamViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ProfileViewModel Manager { get; set; }
+        public ObservableCollection<ProfileViewModel> Workers { get; set; }
+
+        ProfileViewModel selectedWorker;
+        public ProfileViewModel CurrentWorker { get; set; }
+
+        TeamViewModel(ProfileViewModel creator)
+        {
+            this.Manager = creator;
+            this.Workers = new ObservableCollection<ProfileViewModel>();
+            GetWorkersFromDB(this.Manager);
+        }
+
+        private void GetWorkersFromDB(ProfileViewModel manager)
+        {
+            Database.Database.CheckIsCommandManager();
+        }
+
+        TeamViewModel(ProfileViewModel creator, ObservableCollection<ProfileViewModel> workers)
+        {
+            this.Manager = creator;
+            this.Workers = workers;
+        }
+
+
+
+
+
+        public ProfileViewModel SelectedUser
+        {
+            get { return selectedWorker; }
+            set
+            {
+                if (selectedWorker != value)
+                {
+                    selectedWorker = null;
+                    OnPropertyChanged("SelectedWorker");
+                    CurrentWorker = value;
+                    NavigationUtil.Navigation.PushAsync(new ProfilePage(CurrentWorker));
+                }
+            }
+        }
+        protected void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
